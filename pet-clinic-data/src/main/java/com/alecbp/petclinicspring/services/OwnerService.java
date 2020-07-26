d1927acb61be287ad9b38847e29b05a295ac16af
Alec <alec.paglia@gmail.com>
2020-07-26 08:37:19 -0400

    Refactoring services to a command base interface. Closes #11.

--- pet-clinic-data/src/main/java/com/alecbp/petclinicspring/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/alecbp/petclinicspring/services/OwnerService.java
@@ -2,14 +2,8 @@ package com.alecbp.petclinicspring.services;
 
 import com.alecbp.petclinicspring.model.Owner;
 
-import java.util.Set;
+public interface OwnerService extends CrudService<Owner, Long> {
 
-public interface OwnerService {
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/alecbp/petclinicspring/services/VetService.java
+++ pet-clinic-data/src/main/java/com/alecbp/petclinicspring/services/VetService.java
@@ -2,12 +2,6 @@ package com.alecbp.petclinicspring.services;
 
 import com.alecbp.petclinicspring.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
