2671cc48906d30a111842b856ab981e8c9b7c036
Alec <alec.paglia@gmail.com>
2020-07-27 07:58:41 -0400

    Added vets list and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/com/alecbp/petclinicspring/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/alecbp/petclinicspring/controllers/VetController.java
@@ -1,13 +1,25 @@
 package com.alecbp.petclinicspring.controllers;
 
+import com.alecbp.petclinicspring.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
